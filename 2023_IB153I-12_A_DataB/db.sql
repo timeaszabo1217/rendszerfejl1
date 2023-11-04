d8a95a88020c2da763875415c22ae92e194af815
Juhász Ferenc <juhaszferenc08@gmail.com>
2023-11-04 16:38:24 +0100

    src added

--- 2023_IB153I-12_A_DataB/db.sql
+++ 2023_IB153I-12_A_DataB/db.sql
@@ -1,19 +1,19 @@
--- Először ellenőrizze, hogy létezik-e a "Admin" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e a "Admin" t�bla
 DROP TABLE IF EXISTS Admin;
 
--- Most hozza létre a "Admin" táblát újra
+-- Most hozza l�tre a "Admin" t�bl�t �jra
 CREATE TABLE Admin (
-  id serial PRIMARY KEY,
+  admin_id serial PRIMARY KEY,
   admin_username VARCHAR(255) NOT NULL,
   admin_email VARCHAR(255) NOT NULL,
   admin_passw VARCHAR(255) NOT NULL
 );
 
 
--- Először ellenőrizze, hogy létezik-e a "Felhasznalo" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e a "Felhasznalo" t�bla
 DROP TABLE IF EXISTS Felhasznalo;
 
--- Most hozza létre a "Felhasznalo" táblát újra
+-- Most hozza l�tre a "Felhasznalo" t�bl�t �jra
 CREATE TABLE Felhasznalo (
   user_id serial PRIMARY KEY,
   user_username VARCHAR(255) NOT NULL,
@@ -22,10 +22,10 @@ CREATE TABLE Felhasznalo (
   aktiv_leckek VARCHAR(255)
 );
 
--- Először ellenőrizze, hogy létezik-e a "Cikkek" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e a "Cikkek" t�bla
 DROP TABLE IF EXISTS Cikkek;
 
--- Most hozza létre a "Cikkek" táblát újra
+-- Most hozza l�tre a "Cikkek" t�bl�t �jra
 CREATE TABLE Cikkek (
   cikk_id serial PRIMARY KEY,
   cikkek_neve VARCHAR(255) NOT NULL,
@@ -34,10 +34,10 @@ CREATE TABLE Cikkek (
 );
 
 
--- Először ellenőrizze, hogy létezik-e a "Leckek" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e a "Leckek" t�bla
 DROP TABLE IF EXISTS Leckek;
 
--- Most hozza létre a "Leckek" táblát újra
+-- Most hozza l�tre a "Leckek" t�bl�t �jra
 CREATE TABLE Leckek (
   lecke_id serial PRIMARY KEY,
   lecke_neve VARCHAR(255) NOT NULL,
@@ -45,23 +45,23 @@ CREATE TABLE Leckek (
 );
 
 
--- Először ellenőrizze, hogy létezik-e a "FelhasznaloLeckei" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e a "FelhasznaloLeckei" t�bla
 DROP TABLE IF EXISTS FelhasznaloLeckei;
 
--- Most hozza létre a "FelhasznaloLeckei" táblát újra
+-- Most hozza l�tre a "FelhasznaloLeckei" t�bl�t �jra
 CREATE TABLE FelhasznaloLeckei (
   leckek_id INT,
   felhasznalo_id INT,
   PRIMARY KEY (leckek_id, felhasznalo_id),
-  FOREIGN KEY (leckek_id) REFERENCES Lecke (lecke_id),
+  FOREIGN KEY (leckek_id) REFERENCES Leckek (lecke_id),
   FOREIGN KEY (felhasznalo_id) REFERENCES Felhasznalo (user_id)
 );
 
 
--- Először ellenőrizze, hogy létezik-e az "EmailErtesitesek" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e az "EmailErtesitesek" t�bla
 DROP TABLE IF EXISTS EmailErtesitesek;
 
--- Most hozza létre az "EmailErtesitesek" táblát újra
+-- Most hozza l�tre az "EmailErtesitesek" t�bl�t �jra
 CREATE TABLE EmailErtesitesek (
   mail_id serial PRIMARY KEY,
   cimzett_id INT,
@@ -71,10 +71,10 @@ CREATE TABLE EmailErtesitesek (
 );
 
 
--- Először ellenőrizze, hogy létezik-e az "IdopontFoglalas" tábla
+-- Elosz�r ellenorizze, hogy l�tezik-e az "IdopontFoglalas" t�bla
 DROP TABLE IF EXISTS IdopontFoglalas;
 
--- Most hozza létre az "IdopontFoglalas" táblát újra
+-- Most hozza l�tre az "IdopontFoglalas" t�bl�t �jra
 CREATE TABLE IdopontFoglalas (
   idopont_id serial PRIMARY KEY,
   felhasznalo_id INT,
