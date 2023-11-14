d87cd7edadca7a05a32a23d324ef4c940650b850
csabszerko <kiss.csaba.david@gmail.com>
2023-11-15 00:27:09 +0100

    added admin appointments route

--- src/routes/route-admin.js
+++ src/routes/route-admin.js
@@ -23,4 +23,24 @@ router.get("/admin", userAuth, async (req, res) => {
     }
 });
 
+router.get("/admin/appointments", userAuth, async (req, res) => {
+    const token = req.cookies.jwt;
+  
+    var current_role;
+
+    if (token) {
+      jwt.verify(token, jwtSecret, (err, decodedToken) => {
+        current_role = decodedToken.role;
+      });
+    }
+
+    if(current_role !== 'ROLE_ADMIN'){
+        res.send("You're not an admin!")
+    }else{
+        res.render("admin_appointments", {
+            current_role: current_role,
+          });
+    }
+});
+
 module.exports = router;
