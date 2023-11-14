af6bc0f0601ee4001895b16bce83ca4ed65d2a6f
csabszerko <kiss.csaba.david@gmail.com>
2023-11-15 00:30:26 +0100

    updated route

--- src/routes/route-admin.js
+++ src/routes/route-admin.js
@@ -3,26 +3,6 @@ const router = express.Router();
 const { userAuth, jwtSecret } = require("../config/auth.js");
 const jwt = require("jsonwebtoken");
 
-router.get("/admin", userAuth, async (req, res) => {
-    const token = req.cookies.jwt;
-  
-    var current_role;
-
-    if (token) {
-      jwt.verify(token, jwtSecret, (err, decodedToken) => {
-        current_role = decodedToken.role;
-      });
-    }
-
-    if(current_role !== 'ROLE_ADMIN'){
-        res.send("You're not an admin!")
-    }else{
-        res.render("admin", {
-            current_role: current_role,
-          });
-    }
-});
-
 router.get("/admin/appointments", userAuth, async (req, res) => {
     const token = req.cookies.jwt;
   
@@ -38,6 +18,26 @@ router.get("/admin/appointments", userAuth, async (req, res) => {
         res.send("You're not an admin!")
     }else{
         res.render("admin_appointments", {
+            current_role: current_role,
+          });
+    }
+});
+
+router.get("/admin", userAuth, async (req, res) => {
+    const token = req.cookies.jwt;
+  
+    var current_role;
+
+    if (token) {
+      jwt.verify(token, jwtSecret, (err, decodedToken) => {
+        current_role = decodedToken.role;
+      });
+    }
+
+    if(current_role !== 'ROLE_ADMIN'){
+        res.send("You're not an admin!")
+    }else{
+        res.render("admin", {
             current_role: current_role,
           });
     }
