f757edc725b19e9732988c0fe3f3fd2e415fcf40
Durucz Ádám István <h155067@stud.u-szeged.hu>
2023-11-15 14:17:01 +0100

    Biztonsági mentés funkció átírása, DAO-n keresztüli adatlekérdezés

--- src/backupService.js
+++ src/backupService.js
@@ -1,51 +1,63 @@
-const fs = require("fs");
-const path = require("path");
-const pgp = require("pg-promise")();
+const fs = require('fs');
+const path = require('path');
+const pgp = require('pg-promise')();
 
-// Kapcsolódás az adatbázishoz
-const db = pgp("postgres://postgres:1234@localhost:5432/postgres");
+const UserDAO = require("./dao/user-dao.js");
+const LessonDAO = require("./dao/lesson-dao.js");
+const ArticleDAO = require("./dao/article-dao.js");
+const NotificationDAO = require("./dao/notification-dao.js");
+const BookingDAO = require("./dao/booking-dao");
+
+
+const userDAO = new UserDAO();
+const lessonDAO = new LessonDAO();
+const articleDAO = new ArticleDAO();
+const notificationDAO = new NotificationDAO();
+const bookingDAO = new BookingDAO();
+
 
 // Mentési könyvtár elérési útja
-const backupDir = path.join(__dirname, "backup");
+const backupDir = path.join(__dirname, 'backup');
 
 // Ellenőrizze, hogy létezik-e a mentési könyvtár, ha nem, hozza létre
 if (!fs.existsSync(backupDir)) {
   fs.mkdirSync(backupDir);
 }
 
-// Mentés létrehozása az adatbázisból
+// Mentés létrehozása
 async function createBackup() {
-  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
   const backupFile = path.join(backupDir, `backup-${timestamp}.json`);
 
   try {
     // Adatok lekérése az adatbázisból
-    const userData = await db.any('SELECT * FROM Users');
-    const articleData = await db.any('SELECT * FROM Articles');
-    const lessonData = await db.any('SELECT * FROM Lessons');
-    const userLessonData = await db.any('SELECT * FROM UserLessons');
-    const emailNotificationData = await db.any('SELECT * FROM EmailNotifications');
-    const appointmentData = await db.any('SELECT * FROM AppointmentBookings');
+    const userData = await userDAO.getUsers();
+    const articleData = await articleDAO.getArticles();
+    const lessonData = await lessonDAO.getLessons();
+    const notificationData = await notificationDAO.getNotifications();
+    const bookingData = await bookingDAO.getBookings();
 
-    // Fájlba írás
+    // Összeállítja az összes adatot egy objektumban
     const backupData = {
       userData,
       articleData,
       lessonData,
-      userLessonData,
-      emailNotificationData,
-      appointmentData,
+      notificationData,
+      bookingData
     };
 
+    // Az adatok JSON formátumba konvertálása
     const backupContent = JSON.stringify(backupData, null, 2);
-    fs.writeFileSync(backupFile, backupContent);
 
-    console.log("Backup created successfully:", backupFile);
+    // Az adatok írása a megadott fájlba
+    fs.writeFileSync(backupFile, backupContent);
+    console.log('Backup created successfully:', backupFile);
   } catch (error) {
-    console.error("Error creating backup:", error);
+    console.log('Error creating backup:', error);
   }
 }
 
+createBackup();
 module.exports = {
   createBackup,
 };
