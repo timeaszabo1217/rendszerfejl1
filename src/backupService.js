c16096d68b5b66be030ce3385feffae905c2f300
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-14 15:05:11 +0200

    Add files via upload

---
+++ src/backupService.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const pgp = require('pg-promise')();
+
+const UserDAO = require("./dao/user-dao.js");
+const LessonDAO = require("./dao/lesson-dao.js");
+const ArticleDAO = require("./dao/article-dao.js");
+const NotificationDAO = require("./dao/notification-dao.js");
+const BookingDAO = require("./dao/booking-dao");
+
+const userDAO = new UserDAO();
+const lessonDAO = new LessonDAO();
+const articleDAO = new ArticleDAO();
+const notificationDAO = new NotificationDAO();
+const bookingDAO = new BookingDAO();
+
+
+// Mentési könyvtár elérési útja
+const backupDir = path.join(__dirname, 'backup');
+
+// Ellenőrizze, hogy létezik-e a mentési könyvtár, ha nem, hozza létre
+if (!fs.existsSync(backupDir)) {
+  fs.mkdirSync(backupDir);
+}
+
+// Mentés létrehozása
+async function createBackup() {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+  const backupFile = path.join(backupDir, `backup-${timestamp}.json`);
+
+  try {
+    // Adatok lekérése az adatbázisból
+    const userData = await userDAO.getUsers();
+    const articleData = await articleDAO.getArticles();
+    const lessonData = await lessonDAO.getLessons();
+    const notificationData = await notificationDAO.getNotifications();
+    const bookingData = await bookingDAO.getBookings();
+
+    // Összeállítja az összes adatot egy objektumban
+    const backupData = {
+      userData,
+      articleData,
+      lessonData,
+      notificationData,
+      bookingData
+    };
+
+    // Az adatok JSON formátumba konvertálása
+    const backupContent = JSON.stringify(backupData, null, 2);
+
+    // Az adatok írása a megadott fájlba
+    fs.writeFileSync(backupFile, backupContent);
+    console.log('Backup created successfully:', backupFile);
+  } catch (error) {
+    console.log('Error creating backup:', error);
+  }
+}
+
+module.exports = {
+  createBackup,
+};
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const { Pool } = require("pg");
+const app = express();
+const { userAuth } = require("./config/auth.js");
+const routeLesson = require("./routes/route-lessons");
+const routeUser = require("./routes/route-users");
+const routeArticle = require("./routes/route-article");
+const routeBooking = require("./routes/route-booking");
+const schedule = require("node-schedule");
+const backupService = require("./backupService");
+const routeNotification = require("./routes/route-notification");
+const routeProfile = require("./routes/route-profile");
+const routeAdmin = require("./routes/route-admin");
+const PORT = process.env.PORT || 8080;
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(routeArticle);
+app.use(routeLesson);
+app.use(routeBooking);
+app.use(routeUser);
+app.use(routeNotification);
+app.use(routeProfile);
+app.use(routeAdmin);
+app.use('/profile', routeProfile);
+app.use('/profile/edit/password', routeProfile);
+
+app.listen(PORT, () => {
+  console.log("App listening at: http://localhost:8080/");
+});
+
+schedule.scheduleJob("10 3 * * *", () => {
+  backupService.createBackup();
+});
